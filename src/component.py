b2704b230a8e609e336f7288a8e2b61f67aea4ec
Adam Bako <bakoad@gmail.com>
2021-04-23 11:12:58 +0200

    initial commit

---
+++ src/component.py
@@ -0,0 +1,82 @@
+'''
+Template Component main class.
+
+'''
+import logging
+import os
+from pathlib import Path
+
+from keboola.component import CommonInterface
+
+# configuration variables
+KEY_API_TOKEN = '#api_token'
+KEY_PRINT_HELLO = 'print_hello'
+
+# #### Keep for debug
+KEY_DEBUG = 'debug'
+
+# list of mandatory parameters => if some is missing,
+# component will fail with readable message on initialization.
+REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+REQUIRED_IMAGE_PARS = []
+
+APP_VERSION = '0.0.1'
+
+
+def get_local_data_path():
+    return Path(__file__).resolve().parent.parent.joinpath('data').as_posix()
+
+
+def get_data_folder_path():
+    data_folder_path = None
+    if not os.environ.get('KBC_DATADIR'):
+        data_folder_path = get_local_data_path()
+    return data_folder_path
+
+
+class Component(CommonInterface):
+    def __init__(self):
+        # for easier local project setup
+        data_folder_path = get_data_folder_path()
+        super().__init__(data_folder_path=data_folder_path)
+
+        try:
+            # validation of required parameters. Produces ValueError
+            self.validate_configuration(REQUIRED_PARAMETERS)
+            self.validate_image_parameters(REQUIRED_IMAGE_PARS)
+        except ValueError as e:
+            logging.exception(e)
+            exit(1)
+
+        if self.configuration.parameters.get(KEY_DEBUG):
+            self.set_debug_mode()
+
+    @staticmethod
+    def set_debug_mode():
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.info('Running version %s', APP_VERSION)
+        logging.info('Loading configuration...')
+
+    def run(self):
+        '''
+        Main execution code
+        '''
+        params = self.configuration.parameters
+
+        # ####### EXAMPLE TO REMOVE
+        if params.get(KEY_PRINT_HELLO):
+            logging.info("Hello World")
+
+        # ####### EXAMPLE TO REMOVE END
+
+
+"""
+        Main entrypoint
+"""
+if __name__ == "__main__":
+    try:
+        comp = Component()
+        comp.run()
+    except Exception as exc:
+        logging.exception(exc)
+        exit(2)
